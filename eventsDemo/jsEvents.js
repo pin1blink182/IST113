a6ad1b36305fee45a58070b481c177cf870e9f94
pin1blink182 <36709850+pin1blink182@users.noreply.github.com>
2018-02-28 14:15:55 -0500

    jsEvents

--- eventsDemo/jsEvents.js
+++ eventsDemo/jsEvents.js
@@ -1,45 +1,59 @@
 /* 
-   This function creates a popup
+   testEvents sets a variety of listeners to elements so we can see how they work
 */
-function runHello() {
-	alert("Hello User!");
+function testEvents() {
+	// let's grab references to some of the elements in our page
+	let greeting = document.getElementById("greeting");
+	let submit = document.getElementById("submit");
+	let status = document.getElementById("status");
+	let divPage = document.getElementById("page");
+
+	// let's add an event listener and use an anonymous function
+	greeting.addEventListener("mouseover", function() {
+		// we can use JavaScript to change CSS
+		greeting.style.background = "black";
+		greeting.style.color = "white";
+		updateText(greeting);
+	});
+
+	// let's add an event listener and call a function
+	submit.addEventListener("click", myClickFunction);
+	
+	// let's look at bubbling vs capturing propagation
+	// default is false - which is bubbling
+	// change to true to see capturing
+	divPage.addEventListener("click", function() {
+		updateText(divPage);
+	}, false);
 }
 
 /* 
-   This function calls another function
+   myClickFunction can, like any function, be called by an event listener
+   Run this function from the console as well. Does it have the same effect?
 */
-function runOther() {
-	runHello(); 
+function myClickFunction() {
+	let greeting = document.getElementById("greeting");
+	greeting.style.background = "white";
+	greeting.style.color = "black";
+	updateText(greeting);
 }
 
 /*
-   This function prompts the user for a value
-   It then outputs the response to the console
+   removeClick removes our event listener from the submit button
+   Run this manually from the console to test it out
 */
-function runPrompt() {
-	let response = prompt("What is your name?");
-	console.log(response + " just entered their name"); 
+function removeClick() {
+	document.getElementById("status").removeEventListener("click", myClickFunction);
 }
 
-/* 
-   This function has a single parameter
-   It uses the value of that parameter to update the DOM
+/*
+   updateText will help us see how events are being fired
 */
-function changeHtml(changeString) { 
-	document.getElementById("greeting").innerHTML = changeString; 
+function updateText(elem) {
+	document.getElementById("status").innerHTML += "<br/>" + elem.id + " is processing an event!";
 }
 
-/* 
-   This function uses parseInt to turn a user response into an int
-   Returns NaN (not a number) if the user provides bad input
+/*
+   to get things moving, we need to run our testEvents function
 */
-function getNumber() {
-	// we'll request a number (as a string) from the user
-	let numString = prompt("Enter a number:");
-	
-	// lets see if we can turn it into an int
-	let numInt = parseInt(numString);
-	
-	// return the int to the calling function
-	return numInt;
-}
+testEvents();

d84de1eddef3e65905a23fe55cc1c34f922d7008
dickretard <36016241+dickretard@users.noreply.github.com>
2018-02-28 12:36:30 -0500

    Guessing Game JS
    Js game

---
+++ jsGuessingGame/jsGuess.js
@@ -0,0 +1,18 @@
+function GameStart(){
+	var secretnum = prompt("Player one enter a secret number: ");
+	let number = parseInt(secretnum);
+	document.write(number);
+	let guess = prompt("Player two please begin to guess");
+	let guessInt = parseInt(guess);
+	var counter=1;
+	while (guess != number) {
+		if (guess < number){
+			guess = prompt("You've guessed too high! Keep guessing!);
+			counter++;
+		}
+		if (guess > number) {
+			guess = prompt("You've guessed too low! Try again!);
+			counter++;
+		}
+	}
+	alert("Congratulations! It took you " +counter +" guesses");

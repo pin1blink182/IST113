2939158d7ee4c43cc6ba9bae7a1b8e8435b3e7e3
pin1blink182 <36709850+pin1blink182@users.noreply.github.com>
2018-03-21 14:57:14 -0400

    Final Product App Light

--- appLight/appLight.js
+++ appLight/appLight.js
@@ -1,2 +1,17 @@
-function lightSwitch(){
-	let switch = document.getElementById("ON / OFF");
+let lightSwitch = document.getElementById("bswitch");
+let bodyColor = document.getElementById("body");
+let switchOn = true;
+lightSwitch.addEventListener("click", switchToggle);
+
+function switchToggle() {
+  if(switchOn){
+  lightSwitch.style.float = "right";
+  bodyColor.style.background = "black";
+  switchOn = false;
+}
+  else {
+    lightSwitch.style.float = "left";
+    bodyColor.style.background = "white";
+    switchOn = true;
+  }
+}

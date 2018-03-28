3ff049998aeb335893082495951c16387dadf767
pin1blink182 <36709850+pin1blink182@users.noreply.github.com>
2018-03-28 15:50:55 -0400

    Edits

--- appTask1/appTaskJS.js
+++ appTask1/appTaskJS.js
@@ -5,7 +5,6 @@ function checkEmptyInput(){
      var isEmpty = false,
      fname = document.getElementById("fname").value,
      lname = document.getElementById("lname").value,
-     age = document.getElementById("age").value;
 
      if(fname === ""){
           alert("First Name Connot Be Empty");
@@ -13,10 +12,6 @@ function checkEmptyInput(){
      }
      else if(lname === ""){
           alert("Last Name Connot Be Empty");
-          isEmpty = true;
-     }
-     else if(age === ""){
-          alert("Age Connot Be Empty");
           isEmpty = true;
      }
       return isEmpty;
@@ -34,12 +29,10 @@ function checkEmptyInput(){
           cell2 = newRow.insertCell(1),
           cell3 = newRow.insertCell(2),
           fname = document.getElementById("fname").value,
-          lname = document.getElementById("lname").value,
-          age = document.getElementById("age").value;
+          lname = document.getElementById("lname").value;
 
            cell1.innerHTML = fname;
            cell2.innerHTML = lname;
-           cell3.innerHTML = age;
            // call the function to set the event to the new row
            selectedRowToInput();
      }
@@ -53,7 +46,6 @@ function selectedRowToInput() {
                rIndex = this.rowIndex;
                document.getElementById("fname").value = this.cells[0].innerHTML;
                document.getElementById("lname").value = this.cells[1].innerHTML;
-               document.getElementById("age").value = this.cells[2].innerHTML;
                };
           }
 }
@@ -63,12 +55,11 @@ selectedRowToInput();
 
 function editHtmlTbleSelectedRow() {
      var fname = document.getElementById("fname").value,
-     lname = document.getElementById("lname").value,
-     age = document.getElementById("age").value;
+     lname = document.getElementById("lname").value;
           if(!checkEmptyInput()){
                 table.rows[rIndex].cells[0].innerHTML = fname;
                 table.rows[rIndex].cells[1].innerHTML = lname;
-                table.rows[rIndex].cells[2].innerHTML = age;
+
               }
 }
 
@@ -77,5 +68,4 @@ function removeSelectedRow() {
      // clear input text
      document.getElementById("fname").value = "";
      document.getElementById("lname").value = "";
-     document.getElementById("age").value = "";
 }

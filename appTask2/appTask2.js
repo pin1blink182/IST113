e23a69c1d4b6a4a4d57481b883f97b2ccc270240
pin1blink182 <36709850+pin1blink182@users.noreply.github.com>
2018-03-28 16:54:43 -0400

    s

--- appTask2/appTask2.js
+++ appTask2/appTask2.js
@@ -1,1 +1,14 @@
-var myItems = [];
+function testDom(){
+
+  let listOne = document.getElementById("listOne");
+  let inputField = document.getElementById("inputField");
+
+  $("buttonCreate").on("click", function(){
+    let newElem = document.createElement("list")
+    let newContent = document.createTextNode(inputField.value);
+  })
+
+  $("buttonDelete").on("click", function(){
+    listOne.removeChild(newElem);
+  })
+}

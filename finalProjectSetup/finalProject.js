fa5a7fa62826d10e8f35031fa77d56600ac58923
pin1blink182 <36709850+pin1blink182@users.noreply.github.com>
2018-04-18 11:37:17 -0400

    final
    additions

---
+++ finalProjectSetup/finalProject.js
@@ -0,0 +1,26 @@
+//  Create player/high score key pair for local storage
+
+//  Initial page load functions, set up the UI
+function pageLoad() {
+  //  Look for any data stored in local storage
+  //  If any data is stored, load it into the page
+
+}//  End function pageLoad
+
+//  Call the page load function
+pageLoad();
+
+//  Create variables for player names
+let player1Name = $("#player1");
+let player2Name = $("player2");
+
+//  Store player names locally
+//  Create variables for player scores
+//  Create variables for player high scores
+//  Get player 1's name, store it locally
+//  Get player 2's name , store it locally
+//  Create click event to generate random fish size and change display on UI
+//  Have the click event change the clicked squre to indicate it's been used
+//  Also have it put the player's name and size of fish in the square
+//  Create a function to save score to local storage
+//  Check is current score is higher than high score, if so set current to high score
